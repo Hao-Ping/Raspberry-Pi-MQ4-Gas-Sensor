c1eae88756489ccb34f430501675ceb2173f9956
Hao-Ping <haopingchen1999@gmail.com>
2023-04-27 19:51:15 +0800

    The right calibration with RL =1

--- mq4_calibration.py
+++ mq4_calibration.py
@@ -9,7 +9,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1115(i2c)
-ads.gain = 1
+ads.gain = 1 # Full-scale Range +/- 4.096V
 
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
@@ -18,7 +18,7 @@ chan = AnalogIn(ads, ADS.P0)
 #chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 #print("{:>5}\t{:>5}".format('raw', 'v'))
-
+RL = 1 #RL = 1kOhm
 while True:
     #print("{:>5}\t{:>5.5f}".format(chan.value, chan.voltage))
     sensorVolt = 0
@@ -26,7 +26,7 @@ while True:
         sensorVolt += chan.voltage
     sensorVolt /= 500
     print("mean sensorVolt: ", sensorVolt)
-    RS_air = ((5*1) / sensorVolt) - 1
+    RS_air = ((5*RL) / sensorVolt) - RL
     R0 = RS_air / 4.4
     print("R0: ", R0)
     time.sleep(1)
--- mq4_ppm.py
+++ mq4_ppm.py
@@ -19,9 +19,9 @@ chan = AnalogIn(ads, ADS.P0)
 #chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 #print("{:>5}\t{:>5}".format('raw', 'v'))
-m = -0.350
-b = 2.417
-R0 = 3.5
+#m = -0.350
+#b = 2.417
+R0 = 4.55
 
 while True:
     RS_gas = ((5 * 1) / chan.voltage) - 1
@@ -29,5 +29,5 @@ while True:
     #ppm_log = (math.log(ratio, 10)- b) /m
     #ppm = pow(10, ppm_log)
     ppm = 1000*pow(ratio, -2.95)
-    print("ppm: ", ppm)
+    print("ppm: ", ppm, "Voltage: ", chan.voltage)
     time.sleep(1)
